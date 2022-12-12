25437034923f687fef8aa5ea4d00502643e7dd53
Win-Ei-Khaing <wineikhaing111111@gmail.com>
2022-12-11 21:05:57 -0500

    completed lab 8 part 1
    referenced old projects from lab 7

---
+++ Labs/Lab8/Lab8_ByWin/StockService/src/main/java/StockService/StockController.java
@@ -0,0 +1,13 @@
+package StockService;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StockController {
+	@RequestMapping("/stock/{productNumber}")
+	public int getAvailableQuantity(@PathVariable("productNumber") String productNumber) {
+		return 10;
+	}
+}
